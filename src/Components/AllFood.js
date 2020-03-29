60d2cfdf39aaf2957abad08020e20641dff0e03a
givara <givoo25@gmail.com>
2020-03-29 14:32:35 +0200

    working on AllFood  Component

--- src/Components/AllFood.js
+++ src/Components/AllFood.js
@@ -1,9 +1,26 @@
-import React, { useState, useContext } from 'react'
-import { ContextAPI } from './ContextAPI'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 function AllFood() {
-    const [recipes, setRecipes] = useContext(ContextAPI)
+
+    useEffect(() => {
+        getRecipes()
+    }, [])
+
+    const getRecipes = async () => {
+        const response = await fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?number=5&offset=0&query=tomato", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key": "57c6580995msh16a1bdca19a22f4p19d9c9jsn96e04208e387"
+            }
+        })
+        const data = await response.json()
+        setRecipes(data.results)
+        console.log(data.results);
+    }
+
+    const [recipes, setRecipes] = useState([])
 
 
     return (
